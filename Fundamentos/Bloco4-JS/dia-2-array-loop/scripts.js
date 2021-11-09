845f50ba35eb50d27c241db192b6ea8e4223e79d
Pri-Rabelo <priscilarabelo_2002@yahoo.com.br>
2021-11-09 10:16:44 -0300

    adiciona os exercícios do dia 2 de array e loop

---
+++ Fundamentos/Bloco4-JS/dia-2-array-loop/scripts.js
@@ -0,0 +1,116 @@
+// let pizzas = ["4 Queijos", "Frango com Catupiry", "Marguerita", "Palmito", "Chocolate"];
+// pizzas[5] = "Peito de Peru";
+// pizzas.push ("Bacon com Mussarela");
+
+// // console.log ("Menu de sabores: " + pizzas);
+// // // pra saber o tamanho (arquivo). length
+// // console.log (pizzas.length);
+// // console.log (pizzas.sort());
+
+// // for (let index = 0; index < pizzas.length; index += 1){
+// //     console.log (pizzas[index]);
+// // }
+
+// pizzas.unshift ("Mussarela");
+// console.log (pizzas);
+
+// pizzas.pop();
+// console.log (pizzas);
+
+// pizzas.shift();
+// console.log(pizzas);
+
+
+// let groceryList = ["Arroz", "Feijão", "Alface","Melancia"];
+//  console.log (groceryList);
+  
+//  for (let index = 0; index < groceryList.length; index +=1){
+//      console.log (groceryList [index]);
+//  }
+
+// let names = ['João', 'Maria', 'Antonio', ' Margarida'];
+
+// for (let index =0;index < names.length; index +=1){
+//     console.log (names[index]);
+// }
+
+
+// Exercícios dia 2 Array e Loop For
+
+// exercicio 1
+let numeros = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (index = 0;index < numeros.length; index += 1){
+//     console.log(numeros[index]);
+// }
+
+// exercicio 2
+// let soma = 0;
+
+// for (index = 0; index < numeros.length; index +=1){
+//     soma+= numeros[index];
+// }
+// console.log(soma);
+
+// exercicio 3
+// let media = soma / numeros.length;
+// console.log(media);
+
+// //  exercicio 4
+// if (media > 20){
+//     console.log('Valor maior que 20');
+// } else {
+//     console.log('Valor menor que 20');
+// }
+
+// exercicio 5 
+// let maior = numeros[0]
+// for (let index = 0; index < numeros.length; index += 1){
+//     if (numeros[index] > maior){
+//         maior = numeros[index]
+//     } 
+// }
+//  console.log(maior);
+
+
+// exercicio 6
+// let resultado = 0;
+
+// for (index = 0; index < numeros.length; index += 1){
+//     if (numeros[index] % 2 !==0){
+//         resultado +=1;
+//     }
+// }
+
+// if ( resultado === 0){
+//     console.log ('Nenhum valor impar encontrado');
+// } else {
+//     console.log (resultado);
+// }
+
+//  exercicio 7
+// let menor = numeros[0]
+// for (let index = 0; index < numeros.length; index += 1){
+//     if (numeros[index] < menor){
+//         menor = numeros[index]
+//     } 
+// }
+//  console.log(menor);
+
+
+// exercicio 8
+// let numeros = [];
+
+// for (index = 0; index <=25; index += 1){
+//     numeros.push(index);
+// }
+// console.log (numeros);
+
+// // exercicio 9
+// for (index = 0; index < numeros.length; index += 1){
+//     console.log(numeros[index] / 2);
+// }
+
+// BONUS
+
+// exercicio 1
